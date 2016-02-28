46300806475ee7b2aa4342eefa76a204bbfb915b
Christopher Rijos <rijos.christopher@gmail.com>
2016-02-28 09:59:13 -0600

    Added inverse nav bar, for splashscreen

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,15 +8,14 @@ import views.html.*;
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Subscribe"));
+        return ok(index.render("Subscribe", null));
     }
-    //the to do just renders a built in
-    public static Result GO_SUBSCRIBE = redirect(
-            //literally does nothing atm just a place holder
-            //routes.Application.subscribe();
-            String.valueOf(TODO)
-    );
+    //the to do just renders the subscribe page
     public Result subscribe(){
+        return ok(subscribe.render());
+    }
+    //not implemented yet, will render about page
+    public Result about(){
         return TODO;
     }
 
